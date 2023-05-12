aeaf7694f0e0c6bfe77bbef861cde87f91372897
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-05-12 14:35:39 -0300

    Add firebase support

--- TrustlySDKDemoIOS/TrustlySDKDemoIOS/AppDelegate.swift
+++ TrustlySDKDemoIOS/TrustlySDKDemoIOS/AppDelegate.swift
@@ -25,9 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.

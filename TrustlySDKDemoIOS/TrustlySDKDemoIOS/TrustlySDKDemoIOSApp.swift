777d6ef072c9a926ca4114fe9c8354ba251490a4
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-06-12 17:51:38 -0300

    Convert project to work with SwiftUI

---
+++ TrustlySDKDemoIOS/TrustlySDKDemoIOS/TrustlySDKDemoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustlySDKDemoIOSApp.swift
+//  TrustlySDKDemoIOS
+//
+//  Created by Marcos Rivereto on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TrustlySDKDemoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

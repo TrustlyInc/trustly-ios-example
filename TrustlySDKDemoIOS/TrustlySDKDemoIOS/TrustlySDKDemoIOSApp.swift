847d1bad63a4342a2702d2c21011ea4c03a56024
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-06-16 16:36:27 -0300

    Change classe initialization

--- TrustlySDKDemoIOS/TrustlySDKDemoIOS/TrustlySDKDemoIOSApp.swift
+++ TrustlySDKDemoIOS/TrustlySDKDemoIOS/TrustlySDKDemoIOSApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TrustlySDKDemoIOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProductsListView()
         }
     }
 }

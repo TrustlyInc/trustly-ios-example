9c58cb9a1cfbcafffd004b47bdbf2994b11dfa19
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-06-16 16:35:39 -0300

    Create ColorExtension.swift

---
+++ TrustlySDKDemoIOS/TrustlySDKDemoIOS/Extensions/ColorExtension.swift
@@ -0,0 +1,23 @@
+//
+//  ColorExtension.swift
+//  TrustlySDKDemoIOS
+//
+//  Created by Marcos Rivereto on 16/06/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let ui = Color.UI()
+    
+    struct UI {
+        let checkoutButton = Color("CheckoutButton")
+        let checkoutSubtotal = Color("CheckoutSubtotal")
+        let mainTitle = Color("MainTitle")
+        let productDescription = Color("ProductDescription")
+        let productPrice = Color("ProductPrice")
+        let productQuantity = Color("ProductQuantity")
+        let productTitle = Color("ProductTitle")
+        
+    }
+}

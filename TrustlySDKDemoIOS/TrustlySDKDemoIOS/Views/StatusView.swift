81468633e0bb55bf219270b8634dd15cd4fe0528
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-07-03 19:30:11 -0300

    Open and close Transaction detail

--- TrustlySDKDemoIOS/TrustlySDKDemoIOS/Views/StatusView.swift
+++ TrustlySDKDemoIOS/TrustlySDKDemoIOS/Views/StatusView.swift
@@ -10,52 +10,60 @@ import SwiftUI
 struct StatusView<StatusViewModel>: View where StatusViewModel: StatusViewModelProtocol {
         
     @ObservedObject var statusViewModel: StatusViewModel
+    @State private var isPresented = false
     
     var body: some View {
-        VStack{
-            Image(statusViewModel.getStatusImage())
-            Text(statusViewModel.getStatusMessage())
-                .font(.custom("Open Sans", size: 28.0))
-                .fontWeight(.bold)
-                .foregroundColor(Color.ui.productQuantity)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            Button {
-                print("Back to checkout")
-            } label: {
-                Text("Back to checkout")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.ui.checkoutButton)
-                    .border(Color.ui.checkoutButton)
-                    .foregroundColor(.white)
-                    .overlay(RoundedRectangle(cornerRadius: 10)
-                        .stroke(.white, lineWidth: 2)
+        NavigationStack {
+            ZStack{
+                VStack{
+                    Image(statusViewModel.getStatusImage())
+                    Text(statusViewModel.getStatusMessage())
+                        .font(.custom("Open Sans", size: 28.0))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.ui.productQuantity)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Button {
+                        print("Back to checkout")
+                    } label: {
+                        Text("Back to checkout")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.ui.checkoutButton)
+                            .border(Color.ui.checkoutButton)
+                            .foregroundColor(.white)
+                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(.white, lineWidth: 2)
                             )
-                    .cornerRadius(10)
-                    .padding(.leading, 25)
-                    .padding(.trailing, 25)
+                            .cornerRadius(10)
+                            .padding(.leading, 25)
+                            .padding(.trailing, 25)
+                    }
+                    
+                    Button {
+                        isPresented.toggle()
+                    } label: {
+                        Text("Transaction details")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .border(Color.ui.checkoutButton)
+                            .foregroundColor(Color.ui.checkoutButton)
+                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.ui.checkoutButton, lineWidth: 2)
+                            )
+                            .cornerRadius(10)
+                            .padding(.leading, 25)
+                            .padding(.trailing, 25)
+                    }
+                }.navigationBarTitle("Purchase sneakers")
+                    .navigationBarTitleDisplayMode(.inline)
+                
+                TransactionDetailView(viewModel: statusViewModel, show: $isPresented)
             }
             
-            Button {
-                print("Transaction details")
-            } label: {
-                Text("Transaction details")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(.white)
-                    .border(Color.ui.checkoutButton)
-                    .foregroundColor(Color.ui.checkoutButton)
-                    .overlay(RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.ui.checkoutButton, lineWidth: 2)
-                            )
-                    .cornerRadius(10)
-                    .padding(.leading, 25)
-                    .padding(.trailing, 25)
-            }
         }
-        
     }
 }
 
--- TrustlySDKDemoIOS/TrustlySDKDemoIOS/Views/TransactionDetailView.swift
+++ TrustlySDKDemoIOS/TrustlySDKDemoIOS/Views/TransactionDetailView.swift
@@ -7,117 +7,147 @@
 
 import SwiftUI
 
-struct TransactionDetailView<ViewModel>: View where ViewModel: TransactionViewModelProtocol {
+struct TransactionDetailView<ViewModel>: View where ViewModel: StatusViewModelProtocol {
             
     @ObservedObject var viewModel: ViewModel
+    @Binding var show: Bool
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("Transaction details")
-                    .font(.custom("Open Sans", size: 28.0))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.ui.transactionText)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Text("X")
-            }.padding()
-            
-            Divider()
-            
-            HStack {
-                Text("transactionId")
-                    .font(.custom("Open Sans", size: 16.0))
-                    .fontWeight(.regular)
-                    .foregroundColor(Color.ui.transactionLabel)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Text(viewModel.transaction.transactionId)
-                    .font(.custom("Open Sans", size: 16.0))
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.ui.transactionText)
-                    .multilineTextAlignment(.center)
-            }.padding()
-            
-            HStack {
-                Text("transactionType")
-                    .fontWeight(.regular)
-                    .foregroundColor(Color.ui.transactionLabel)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Text(viewModel.transaction.transactionType)
-            }.padding(.leading)
-                .padding(.trailing)
-                .padding(.bottom)
-            
-            HStack {
-                Text("paymentId")
-                    .fontWeight(.regular)
-                    .foregroundColor(Color.ui.transactionLabel)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Text(viewModel.transaction.paymentId)
-                    .font(.custom("Open Sans", size: 16.0))
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.ui.transactionText)
-                    .multilineTextAlignment(.center)
-            }.padding(.leading)
-                .padding(.trailing)
-                .padding(.bottom)
-            
-            HStack {
-                Text("paymentType")
-                    .fontWeight(.regular)
-                    .foregroundColor(Color.ui.transactionLabel)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Text(viewModel.transaction.paymentType)
-                    .font(.custom("Open Sans", size: 16.0))
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.ui.transactionText)
-                    .multilineTextAlignment(.center)
-            }.padding(.leading)
-                .padding(.trailing)
-                .padding(.bottom)
-            
-            HStack {
-                Text("status")
-                    .fontWeight(.regular)
-                    .foregroundColor(Color.ui.transactionLabel)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Text(viewModel.transaction.status)
-                    .font(.custom("Open Sans", size: 16.0))
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.ui.transactionText)
-                    .multilineTextAlignment(.center)
-            }.padding(.leading)
-                .padding(.trailing)
-                .padding(.bottom)
-            
-            Divider()
-            
-            Button {
-                print("Close")
-            } label: {
-                Text("Close")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.ui.checkoutButton)
-                    .border(Color.ui.checkoutButton)
-                    .foregroundColor(.white)
-                    .overlay(RoundedRectangle(cornerRadius: 10)
-                        .stroke(.white, lineWidth: 2)
-                            )
-                    .cornerRadius(10)
-                    .padding()
+        
+        ZStack {
+            if show {
+                if let transaction = viewModel.transaction {
+                    Color.black.opacity(show ? 0.3 : 0).edgesIgnoringSafeArea(.all)
+                    VStack {
+                        HStack {
+                            Text("Transaction details")
+                                .font(.custom("Open Sans", size: 28.0))
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.ui.transactionText)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                            Text("X")
+                                .font(.custom("Open Sans", size: 18))
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.ui.transactionText)
+                                .multilineTextAlignment(.center)
+                                .onTapGesture {
+                                    show.toggle()
+                            }
+                        }.padding()
+
+                        Divider()
+
+                        HStack {
+                            Text("transactionId")
+                                .font(.custom("Open Sans", size: 16.0))
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.ui.transactionLabel)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                            Text(transaction.transactionId)
+                                .font(.custom("Open Sans", size: 16.0))
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.ui.transactionText)
+                                .multilineTextAlignment(.center)
+                        }.padding()
+
+                        HStack {
+                            Text("transactionType")
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.ui.transactionLabel)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                            Text(transaction.transactionType.description())
+                        }.padding(.leading)
+                            .padding(.trailing)
+                            .padding(.bottom)
+
+                        HStack {
+                            Text("paymentId")
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.ui.transactionLabel)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                            Text(transaction.paymentId ?? "")
+                                .font(.custom("Open Sans", size: 16.0))
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.ui.transactionText)
+                                .multilineTextAlignment(.center)
+                        }.padding(.leading)
+                            .padding(.trailing)
+                            .padding(.bottom)
+
+                        HStack {
+                            Text("paymentType")
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.ui.transactionLabel)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                            Text(transaction.paymentType?.description() ?? "")
+                                .font(.custom("Open Sans", size: 16.0))
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.ui.transactionText)
+                                .multilineTextAlignment(.center)
+                        }.padding(.leading)
+                            .padding(.trailing)
+                            .padding(.bottom)
+
+                        HStack {
+                            Text("status")
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.ui.transactionLabel)
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                            Text(transaction.status.description())
+                                .font(.custom("Open Sans", size: 16.0))
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.ui.transactionText)
+                                .multilineTextAlignment(.center)
+                        }.padding(.leading)
+                            .padding(.trailing)
+                            .padding(.bottom)
+
+                        Divider()
+
+                        Button {
+                            show.toggle()
+                        } label: {
+                            Text("Close")
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.ui.checkoutButton)
+                                .border(Color.ui.checkoutButton)
+                                .foregroundColor(.white)
+                                .overlay(RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.white, lineWidth: 2)
+                                        )
+                                .cornerRadius(10)
+                                .padding()
+                        }
+                    }.foregroundColor(.white)
+                        .background(.white)
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(.white, lineWidth: 2)
+                                )
+                        .cornerRadius(10)
+                        .padding(.leading, 25)
+                        .padding(.trailing, 25)
+
+                } else {
+                    Text("Invalid transaction")
+                }
             }
         }
     }
 }
 
 struct TransactionDetailView_Previews: PreviewProvider {
+    static var transaction = Transaction(transactionId: "123456", transactionType: .External, status: .Authorized, panel: "1", url: "", merchantReference: "", requestSignature: "", paymentId: "654321", paymentType: .Retrieval, paymentProviderType: "", paymentAccountVerified: "")
+    
+    @State static var show = true
+    
     static var previews: some View {
-        TransactionDetailView(viewModel: TransactionViewModel())
+        TransactionDetailView(viewModel: StatusViewModel(transaction: transaction), show: $show)
     }
 }
